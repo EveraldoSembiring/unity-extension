a653d83a2f84691475aeb6e80caebc8b0648c0d5
Everaldo <everaldo@accelbyte.net>
2025-02-04 20:39:59 +0700

    feat: add pick multiple random weight

--- Runtime/Randomizer/RandomWithWeight.cs
+++ Runtime/Randomizer/RandomWithWeight.cs
@@ -55,6 +55,88 @@ namespace UnityExtension
 
             return result;
         }
+        
+        public static List<T> PickMultiple<T>(this List<WeightRandom<T>> randomCandidates, uint pickCount, bool unique)
+        {
+            if (randomCandidates == null && randomCandidates.Count == 0)
+            {
+                return null;
+            }
+
+            var retval = new List<T>();
+            
+            if (randomCandidates.Count == 1)
+            {
+                if (randomCandidates[0].Weight > 0)
+                {
+                    if (unique)
+                    {
+                        retval.Add(randomCandidates[0].Object);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < pickCount; i++)
+                        {
+                            retval.Add(randomCandidates[0].Object);         
+                        }
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            float totalWeight = 0;
+            foreach (WeightRandom<T> candidate in randomCandidates)
+            {
+                if (candidate.Weight > 0)
+                {
+                    float rangeMin = totalWeight;
+                    float rangeMax = totalWeight + candidate.Weight;
+                    totalWeight = rangeMax;
+                    candidate.Range = new Vector2(rangeMin, rangeMax);
+                }
+            }
+
+            if (totalWeight == 0)
+            {
+                return null;
+            }
+
+            while (retval.Count < pickCount)
+            {
+                float random = UnityEngine.Random.Range(0, totalWeight);
+                if (randomCandidates.Count > 1)
+                {
+                    WeightRandom<T> pickedRandomObject = randomCandidates.Find(candidate =>
+                        candidate.Range != null && candidate.Range.Value.x < random &&
+                        random <= candidate.Range.Value.y);
+                    if (pickedRandomObject != null)
+                    {
+                        retval.Add(pickedRandomObject.Object);
+                        if (unique)
+                        {
+                            randomCandidates.Remove(pickedRandomObject);
+                        }
+                    }
+                }
+                else if (randomCandidates.Count == 1)
+                {
+                    retval.Add(randomCandidates[0].Object);
+                    if (unique)
+                    {
+                        randomCandidates.RemoveAt(0);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return retval;
+        }
     }
 
     public class WeightRandom<T>
